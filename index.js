8f7a115b5b8cf7a75586e1acf8f8b09188d6ec99
Roberley Cuadra <roberleyc91@gmail.com>
2020-12-02 06:02:05 -0500

    edit 2

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const { showAlert } = require("./alerts");
 const { logText } = require("./log");
+// let cant = process.argv.slice(2);
 
-let cant = process.argv.slice(2);
-cant = (cant.length === 0) ? 30 : cant;
+const [nodeP, dirP, timeP] = process.argv;
 
-logText(`Timer configured for: ${cant} minutes.`, 'Soft');
+let cant = timeP || 30;
+
+logText(`${cant} minutes.`, 'Soft');
 
 logText(cant, 'Electronic');
 const TIMER = setInterval(() => {
   if (--cant === 0) {
-    showAlert('Time done!!!', `past ${cant} minutes` );
+    showAlert('Time done', `past ${cant} minutes` );
     logText(cant, 'Electronic');
     logText(`Timer Done!!!`, 'Soft');
     clearInterval(TIMER);
